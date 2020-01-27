333531217b0196136912640660403d3645326bff
zerckel <antoinecarbonnel@yahoo.fr>
2020-01-27 12:57:42 +0100

    first commit

--- js.js
+++ js.js
@@ -1,27 +1,3 @@
-Vue.component('card', {
-  name: 'card',
-  props: ['card'],
-  template: `<span>
-            <div v-on:click="update(card.id)" class="note">
-                <span> {{card.text}} </span>
-                <span> {{card.text.length}}</span>
-            </div>
-            <divgit  v-on:click="deleteNote(card.id)" class="delete">
-                DELETE NOTE
-            </divgit>
-            <textarea v-if="card.modify"  class="modify" v-model="card.text"></textarea>
-            </span>  
-`,
-  methods:{
-    update: function (elem) {
-      this.$emit('update-note', elem)
-    },
-    deleteNote: function (elem) {
-      this.$emit('delete-note', elem)
-    }
-  }
-})
-
 let app = new Vue({
   el: '#app',
   data: {
@@ -29,34 +5,11 @@ let app = new Vue({
   },
   methods:{
     create:function () {
-      let nbr = this.cards.length
         this.cards.push({
-          id: nbr,
           text: 'empty note',
-          modify: false,
-          countWord: 0
+          modif: false
         })
-      localStorage.setItem('cards', JSON.stringify(this.cards))
-    },
-    deleteNote: function (elem) {
-      this.cards[elem].text = false
-      localStorage.setItem('cards', JSON.stringify(this.cards))
-    },
-    update: function(elem){
-      console.log('ok')
-      if (this.cards[elem].modify === true){
-        this.cards[elem].modify = false
-      }else if (this.cards[elem].modify === false){
-        this.cards[elem].modify = true
-      }
-      localStorage.setItem('cards', JSON.stringify(this.cards))
-    }
-
-  },
-  mounted:function () {
-    if (localStorage.getItem('cards') !== null){
-      this.cards = JSON.parse(localStorage.getItem('cards'))
     }
   }
-})
 
+})
